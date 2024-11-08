5e86ead127e4f1bc631dafb0d28c2b2b412c1116
31DPGGarais <131352750+GustavsGarais@users.noreply.github.com>
2024-11-08 14:18:35 +0000

    aaaaaaa

--- index.js
+++ index.js
@@ -6,34 +6,28 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-// places the marker on the first shown place -> Riga
-var marker = L.marker([56.9467, 24.1203]).addTo(map);
-marker.bindPopup("<b>Rīga</b><br>Rīgas Centrālā stacija").openPopup();
 
-// defines the source and destination coordinate systems
 var sourceProj = 'EPSG:3059';
 var destProj = 'EPSG:4326';
 
-// fetches a json file
 fetch('dati.json')
     .then(response => response.json())
     .then(data => {
-        console.log('JSON data:', data); // Debug log
+        console.log('JSON data:', data); 
 
-        // loops through the features array
         data.features.forEach(function(feature) {
-            // extracts coordinates and properties
+
             var coordinates = feature.geometry.coordinates;
             var properties = feature.properties;
 
-            console.log('Original coordinates:', coordinates); // Debug log
+            console.log('Original coordinates:', coordinates); 
 
-            // transforms the coordinates
+
             var transformedCoordinates = proj4(sourceProj, destProj, coordinates);
 
-            console.log('Transformed coordinates:', transformedCoordinates); // Debug log
+            console.log('Transformed coordinates:', transformedCoordinates);
 
-            // adds a marker for each feature
+
             L.marker([transformedCoordinates[1], transformedCoordinates[0]]).addTo(map)
                 .bindPopup(properties.PLACENAME);
         });
